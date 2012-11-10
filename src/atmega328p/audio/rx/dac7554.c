1ddb0fe5147a9b5f897c5d27e33bea0a4f7427f5
Fabien Le Mentec <texane@gmail.com>
2012-11-10 12:44:34 -0600

    [ update ] dac7554 uses spi_xxx_cpol

--- src/atmega328p/audio/rx/dac7554.c
+++ src/atmega328p/audio/rx/dac7554.c
@@ -2,33 +2,31 @@
 #include <avr/io.h>
 #include "../../common/spi.c"
 
+
 /* reference: SLAS399A.pdf */
 
-static inline void dac7554_spi_cs_low(void)
+static inline void dac7554_sync_low(void)
 {
-#define DAC7554_SPI_CS_MASK (1 << 2)
-  PORTD &= ~DAC7554_SPI_CS_MASK;
+#define DAC7554_SYNC_MASK (1 << 0)
+  PORTB &= ~DAC7554_SYNC_MASK;
 }
 
-static inline void dac7554_spi_cs_high(void)
+static inline void dac7554_sync_high(void)
 {
-  PORTD |= DAC7554_SPI_CS_MASK;
+  PORTB |= DAC7554_SYNC_MASK;
 }
 
 static void dac7554_setup(void)
 {
-  /* assume spi initialized */
-#if 0
-  /* spi_setup_master() */
-  /* spi_setup_master(SPI_SCK_FREQ_FOSC2); */
-#endif
-
-  DDRD |= DAC7554_SPI_CS_MASK;
-  dac7554_spi_cs_high();
+  /* sync pin controled with PB0, 8 */
+  DDRB |= DAC7554_SYNC_MASK;
+  dac7554_sync_high();
 }
 
 static inline void dac7554_write(uint16_t data, uint16_t chan)
 {
+  /* assume spi initialized */
+
   /* refer to SLAS399A, p.14 table.1 for commands */
 
   /* assume: 0 <= data < 4096 */
@@ -37,14 +35,24 @@ static inline void dac7554_write(uint16_t data, uint16_t chan)
   /* 2 to update both input and DAC registers */  
   const uint16_t cmd = (2 << 14) | (chan << 12) | data;
 
-  dac7554_spi_cs_low();
+  const uint8_t cpol = spi_set_cpol();
+
+  /* pulse sync */
+  dac7554_sync_high();
+  __asm__ __volatile__ ("nop");
+  __asm__ __volatile__ ("nop");
+  dac7554_sync_low();
 
   /* wait for SCLK falling edge setup time, 4ns */
+  __asm__ __volatile__ ("nop");
   __asm__ __volatile__ ("nop");
 
   spi_write_uint16(cmd);
 
   /* wait for SLCK to rise (0 ns) and set high */
   __asm__ __volatile__ ("nop");
-  dac7554_spi_cs_high();
+  __asm__ __volatile__ ("nop");
+  dac7554_sync_high();
+
+  spi_restore_cpol(cpol);
 }
