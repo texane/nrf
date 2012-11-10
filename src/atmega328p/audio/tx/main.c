dacdb7119dc972cf23fdbfc934860f0822f3caf8
Fabien Le Mentec <texane@gmail.com>
2012-11-10 01:51:42 -0600

    [ add ] audio/tx

---
+++ src/atmega328p/audio/tx/main.c
@@ -0,0 +1,108 @@
+#include <stdint.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "../../common/spi.c"
+#include "../../common/nrf24l01p.c"
+#include "../../common/uart.c"
+
+
+/* timer1a compare on match handler */
+
+static volatile uint16_t counter;
+
+ISR(TIMER1_COMPA_vect)
+{
+  ++counter;
+}
+
+
+int main(void)
+{
+  uart_setup();
+
+  uart_write((uint8_t*)"tx side\r\n", 9);
+
+  nrf24l01p_setup();
+
+  /* sparkfun usb serial board configuration */
+  /* NOTE: nrf24l01p_enable_crc8(); for nrf24l01p board */
+  /* nrf24l01p_enable_crc16(); */
+  nrf24l01p_disable_crc();
+  /* auto ack disabled */
+  /* auto retransmit disabled */
+  /* 4 bytes payload */
+  /* 1mbps, 0dbm */
+  /* nrf24l01p_set_rate(NRF24L01P_RATE_1MBPS); */
+  nrf24l01p_set_rate(NRF24L01P_RATE_2MBPS);
+  /* nrf24l01p_set_rate(NRF24L01P_RATE_250KBPS); */
+  /* channel 2 */
+  nrf24l01p_set_chan(2);
+  /* 5 bytes addr width */
+  /* nrf24l01p_set_addr_width(NRF24L01P_ADDR_WIDTH_5); */
+  nrf24l01p_set_addr_width(NRF24L01P_ADDR_WIDTH_3);
+  /* rx address */
+  nrf24l01p_cmd_buf[0] = 0xe7;
+  nrf24l01p_cmd_buf[1] = 0xe7;
+  nrf24l01p_cmd_buf[2] = 0xe7;
+  nrf24l01p_cmd_buf[3] = 0xe7;
+  nrf24l01p_cmd_buf[4] = 0xe7;
+  nrf24l01p_write_reg40(NRF24L01P_REG_RX_ADDR_P0);
+  /* tx address */
+  nrf24l01p_cmd_buf[0] = 0xe7;
+  nrf24l01p_cmd_buf[1] = 0xe7;
+  nrf24l01p_cmd_buf[2] = 0xe7;
+  nrf24l01p_cmd_buf[3] = 0xe7;
+  nrf24l01p_cmd_buf[4] = 0xe7;
+  nrf24l01p_write_reg40(NRF24L01P_REG_TX_ADDR);
+  /* enable tx no ack command */
+  nrf24l01p_enable_tx_noack();
+
+  nrf24l01p_powerdown_to_standby();
+
+  uart_write((uint8_t*)"tx side\r\n", 9);
+  uart_write((uint8_t*)"press space\r\n", 13);
+  uart_read_uint8();
+  uart_write((uint8_t*)"starting\r\n", 10);
+
+  /* setup counter */
+  counter = 0;
+
+  /* clock source disabled, safe to access 16 bits counter */
+  /* setup timer1, ctc mode, interrupt on match 400 */
+  TCNT1 = 0;
+  OCR1A = 400;
+  OCR1B = 0xffff;
+  TCCR1A = 0;
+  TCCR1C = 0;
+  TIMSK1 = 1 << 1;
+  /* prescaler set to 1, interrupt on match 400 (40khz) */
+  TCCR1B = (1 << 3) | (1 << 0);
+
+  /* enable interrupts */
+  sei();
+
+#if 0
+  while (1)
+  {
+    nrf24l01p_standby_to_tx();
+    if (nrf24l01p_is_tx_empty() == 0) nrf24l01p_flush_tx();
+
+    nrf24l01p_write_tx_noack();
+    while (nrf24l01p_is_tx_irq() == 0) ;
+  }
+#else
+  while (1)
+  {
+    if (counter >= 40000)
+    {
+      cli();
+      counter = 0;
+      sei();
+
+      uart_write((uint8_t*)"x\r\n", 3);
+    }
+  }
+#endif
+
+  return 0;
+}
