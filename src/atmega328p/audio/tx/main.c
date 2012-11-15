cd3fdc158ddf92c103a34fa766d8601475b6d785
lementec <lementec@esrf.fr>
2012-11-15 11:42:25 +0100

    [ update, wip ] use adc in tx

--- src/atmega328p/audio/tx/main.c
+++ src/atmega328p/audio/tx/main.c
@@ -4,12 +4,21 @@
 #include "../../common/spi.c"
 #include "../../common/nrf24l01p.c"
 #include "../../common/uart.c"
+
+
+/* data input source */
+
+#define CONFIG_INPUT_ADC 1
+
+#if (CONFIG_INPUT_ADC == 1)
+
 #include "adc.c"
 
+#else
 
 /* tone generated array */
 
-static const uint8_t sample_array[256] =
+static const uint8_t tone_array[256] =
 {
 #if 0
 # include "../tone/tone_40000_1000.c"
@@ -24,6 +33,11 @@ static const uint8_t sample_array[256] =
 #endif
 };
 
+static uint8_t tone_index = 0;
+
+#endif
+
+static uint8_t sample_buffer[NRF24L01P_PAYLOAD_WIDTH];
 static uint8_t sample_index;
 
 
@@ -31,17 +45,22 @@ static uint8_t sample_index;
 
 ISR(TIMER1_COMPA_vect)
 {
-  uint8_t i;
+  /* capture next sample */
 
-  for (i = 0; i < NRF24L01P_PAYLOAD_WIDTH; ++i)
+#if (CONFIG_INPUT_ADC == 1)
+  const uint8_t x = (uint8_t)(adc_read() >> 2);
+#else
+  const uint8_t x = tone_array[tone_index++];
+#endif
+
+  sample_buffer[sample_index++] = x;
+
+  if (sample_index == NRF24L01P_PAYLOAD_WIDTH)
   {
-    nrf24l01p_cmd_buf[i] = sample_array[sample_index];
-    /* automatic index wrapping */
-    ++sample_index;
+    sample_index = 0;
+    nrf24l01p_write_tx_noack_zero(sample_buffer);
+    while (nrf24l01p_is_tx_irq() == 0) ;
   }
-
-  nrf24l01p_write_tx_noack_zero(sample_array + sample_index);
-  while (nrf24l01p_is_tx_irq() == 0) ;
 }
 
 
@@ -97,18 +116,23 @@ int main(void)
   uart_read_uint8();
   uart_write((uint8_t*)"starting\r\n", 10);
 
-  /* sample index */
   sample_index = 0;
+
+#if (CONFIG_INPUT_ADC == 1)
+  /* enable before sampling interrupt */
+  adc_setup();
+  adc_start_free_running();
+#endif
 
   /* clock source disabled, safe to access 16 bits counter */
   /* setup timer1, ctc mode, interrupt on match 400 */
   TCNT1 = 0;
-  OCR1A = 12800;
+  OCR1A = 400;
   OCR1B = 0xffff;
   TCCR1A = 0;
   TCCR1C = 0;
   TIMSK1 = 1 << 1;
-  /* base freq is 40khz, 32 samples per int gives 1.25khz */
+  /* sample at 40khz, send every 32 bytes */
   /* prescaler set to 1, autoclear on interrupt on match */
   TCCR1B = (1 << 3) | (1 << 0);
 
