12577cac3d68e65b79c76a3b8bb3759122a9e72e
lementec <lementec@esrf.fr>
2012-11-15 11:00:58 +0100

    [ update ] implement adc

--- src/atmega328p/audio/tx/adc.c
+++ src/atmega328p/audio/tx/adc.c
@@ -9,19 +9,52 @@
 
 static inline void adc_setup(void)
 {
+  /* this routine must be called once and before the others */
+
+  DDRC &= ~ADC_MASK;
+
+  /* 10 bits resolution requires a clock between 50khz and 200khz. */
+  /* a prescaler is used to derive fadc from fcpu. the prescaler */
+  /* starts counting when ADEN is set, and is always reset when */
+  /* ADEN goes low. */
+
+  /* 16mhz / 200khz = 80. prescaler set to 128, thus fadc = 125khz */
+  /* a normal conversion takes 13 FADC cycles */
+  /* thus, actual sampling rate of 125000 / 13 = 9615 samples per second */
+
+  ADCSRA = (7 << ADPS0);
+  ADCSRB = 0;
+
+  /* aref, internal vref off, channel 0 */
+  ADMUX = 0;
 }
 
-static void adc_start_read(void)
+static inline void adc_wait_25(void)
 {
+  /* wait for 25 adc cycles */
+  /* FADC = FCPU / 128, thus waits for 128 * 25 = 3200 cpu cycles */
+  uint8_t x = 0xff;
+  for (; x; --x) __asm__ __volatile__ ("nop\n\t");
 }
 
-static uint16_t adc_wait_read(void)
+static void adc_start_free_running(void)
 {
-  return 0;
+  /* enable adc and free running mode */
+  ADCSRB = 0;
+  ADCSRA |= (1 << ADEN) | (1 << ADATE);
+
+  /* the first conversion starts 25 FADC cycles after ADEN set */
+  adc_wait_25();
+}
+
+static inline void adc_stop(void)
+{
+  ADCSRA &= ~(1 << ADEN);
 }
 
 static inline uint16_t adc_read(void)
 {
-  adc_start_read();
-  return adc_wait_read();
+  /* read adcl first */
+  const uint8_t l = ADCL;
+  return (((uint16_t)ADCH) << 8) | (uint16_t)l;
 }
