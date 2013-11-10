a70820e6cf9a4fe83f8c9f1ab76bd1a91ef6b8aa
texane <texane@gmail.com>
2013-11-10 09:17:12 -0600

    [ update ] debug msg, info key

--- src/atmega328p/snrf/src/common/snrf_common.h
+++ src/atmega328p/snrf/src/common/snrf_common.h
@@ -6,23 +6,27 @@
 #define SNRF_OP_GET 1
 #define SNRF_OP_PAYLOAD 2
 #define SNRF_OP_COMPL 3
+#define SNRF_OP_DEBUG 4
 
-#define SNRF_KEY_STATE 0
-#define SNRF_KEY_CRC 1
-#define SNRF_KEY_RATE 2
-#define SNRF_KEY_CHAN 3
-#define SNRF_KEY_ADDR_WIDTH 4
-#define SNRF_KEY_RX_ADDR 5
-#define SNRF_KEY_TX_ADDR 6
-#define SNRF_KEY_ACK 7
-#define SNRF_KEY_PAYLOAD_WIDTH 8
+#define SNRF_KEY_INFO 0
+#define SNRF_KEY_STATE 1
+#define SNRF_KEY_CRC 2
+#define SNRF_KEY_RATE 3
+#define SNRF_KEY_CHAN 4
+#define SNRF_KEY_ADDR_WIDTH 5
+#define SNRF_KEY_RX_ADDR 6
+#define SNRF_KEY_TX_ADDR 7
+#define SNRF_KEY_ACK 8
+#define SNRF_KEY_PAYLOAD_WIDTH 9
 
 #define SNRF_STATE_IDLE 0
 #define SNRF_STATE_TXRX 1
 
 #define SNRF_ERR_SUCCESS 0
 #define SNRF_ERR_FAILURE 1
-#define SNRF_ERR_OVERFLOW 2
+#define SNRF_ERR_OP 2
+#define SNRF_ERR_KEY 3
+#define SNRF_ERR_VAL 4 
 
 typedef struct
 {
@@ -53,6 +57,13 @@ typedef struct
       uint8_t err;
       uint32_t val;
     } compl;
+
+    struct
+    {
+      uint32_t data;
+      uint32_t line;
+    } debug;
+
   } u;
 
 } __attribute__((packed)) snrf_msg_t;
