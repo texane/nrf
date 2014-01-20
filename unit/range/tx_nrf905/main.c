edce06a40113640e3ece58d488425945e0ad9e42
texane <texane@gmail.com>
2014-01-19 22:01:55 -0600

    [ fix ] tx_nrf905 delay

--- unit/range/tx_nrf905/main.c
+++ unit/range/tx_nrf905/main.c
@@ -10,19 +10,24 @@ static void make_pattern(void)
   uint8_t i;
   uint8_t x;
 
-  for (i = 0, x = 0x2a; i < nrf905_payload_width; ++i, ++x)
+  for (i = 0, x = 0x2a; i != nrf905_payload_width; ++i, ++x)
     nrf905_payload_buf[i] = x;
+}
+
+static void wait(void)
+{
+  volatile uint16_t x;
+  volatile uint16_t xx;
+
+  for (xx = 0; xx < 5; ++xx)
+    for (x = 0; x != (uint16_t)0xffff; ++x)
+      __asm__ __volatile__ ("nop");
 }
 
 int main(void)
 {
   uint8_t tx_addr[3] = { 0x01, 0x02, 0x03 };
   uint8_t rx_addr[3] = { 0x0a, 0x0b, 0x0c };
-
-  uint32_t counter = 0;
-#if 0
-  uint8_t x;
-#endif
 
   /* setup spi first */
   spi_setup_master();
@@ -37,18 +42,23 @@ int main(void)
   nrf905_set_standby();
 
 #if 0
-  uart_write((uint8_t*)"tx side\r\n", 9);
-  uart_write((uint8_t*)"press space\r\n", 13);
-  uart_read_uint8(&x);
-  uart_write((uint8_t*)"starting\r\n", 10);
+  {
+    uint8_t x;
+    uart_write((uint8_t*)"tx side\r\n", 9);
+    uart_write((uint8_t*)"press space\r\n", 13);
+    uart_read_uint8(&x);
+    uart_write((uint8_t*)"starting\r\n", 10);
+  }
 #endif
 
   while (1)
   {
-    if ((++counter) != (200000UL / 8UL)) continue ;
-    counter = 0;
+    wait();
+
     make_pattern();
     nrf905_write_payload();
+
+    uart_write((uint8_t*)"x\r\n", 3);
   }
 
   return 0;
