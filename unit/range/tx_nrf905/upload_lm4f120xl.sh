27b28c54660fbf8eb93e0095da544b690e2231d1
texane <texane@gmail.com>
2014-01-20 21:41:20 -0600

    [ fix ] upload_lm4f120xl.sh

--- unit/range/tx_nrf905/upload_lm4f120xl.sh
+++ unit/range/tx_nrf905/upload_lm4f120xl.sh
@@ -3,7 +3,7 @@
 HOSTNAME=`hostname`
 case $HOSTNAME in
 debian)
- LM4F120XL_DIR=$HOME/repo/stella
+ LM4F120XL_DIR=/home/texane/repo/stella
  ;;
 *)
  LM4F120XL_DIR=/buffer/PCLAB2451/lementec/repo/lm4f120xl
